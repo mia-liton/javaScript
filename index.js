c0d17f2d41f878d3dbb3ca9b9e43184a08b55be4
liton <litonmioa@gmail.com>
2026-02-16 15:52:59 +0600

    use the ternary operator to check if a number is even or odd

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
-let a = 5;
-let b = "5";
+let num = 5;
 
-// console.log(a == b);
-// console.log(a === b);
+let result = (num % 2 === 0) ? "even" : "odd";
 
-let c = null;
-let d;
-
-// console.log(c == d);
-// console.log(c === d);
-
-let e = 1;
-let f = true;
-
-// console.log(e == f);
-// console.log(e === f);
-
-let g = "hello";
-let h = "hello";
-
-// console.log(g == h);
-// console.log(g === h);
-
-let obj1 = {name: "Liton"};
-let obj2 = {name: "Liton"};
-let obj3 = obj1;
-
-console.log(obj1 == obj2);
-console.log(obj1 === obj2);
-console.log(obj1 === obj3);
+console.log(result);
