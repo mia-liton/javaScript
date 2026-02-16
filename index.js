256313b1370670b3d496d7725ddd1b7a959b4f52
liton <litonmioa@gmail.com>
2026-02-16 15:49:08 +0600

    compare == vs === with five different examples

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-let a = 10;
-let b = 3;
+let a = 5;
+let b = "5";
 
-console.log("a =", a, ", b =", b);
-console.log("Addition: a + b = ", a + b);
-console.log("Subtraction: a - b = ", a - b);
-console.log("Multiplication: a * b = ", a * b);
-console.log("Division: a / b = ", a / b);
-console.log("Remainder: a % b = ", a % b);
-console.log("Exponentiation: a ** b = ", a ** b);
+// console.log(a == b);
+// console.log(a === b);
 
-a++;
-console.log("Increment: a++ = ", a);
+let c = null;
+let d;
 
-b--;
-console.log("Decrement: b-- = ", b);
+// console.log(c == d);
+// console.log(c === d);
+
+let e = 1;
+let f = true;
+
+// console.log(e == f);
+// console.log(e === f);
+
+let g = "hello";
+let h = "hello";
+
+// console.log(g == h);
+// console.log(g === h);
+
+let obj1 = {name: "Liton"};
+let obj2 = {name: "Liton"};
+let obj3 = obj1;
+
+console.log(obj1 == obj2);
+console.log(obj1 === obj2);
+console.log(obj1 === obj3);
