563b7cebc11a62a9f04e4a4d3e2cf6a1fbdcbe35
liton <litonmioa@gmail.com>
2026-02-16 15:04:17 +0600

    checking if a variable is null,undefined, or has a value

--- declarevariable.js
+++
@@ -1,3 +0,0 @@
-var name = "Liton";
-let age = 25;
-const country = "Bangladesh";
---
+++ index.js
@@ -0,0 +1,13 @@
+let data;
+
+if (data === null) {
+  console.log("Variable is null");
+}
+
+if (data === undefined) {
+  console.log("Variable is undefined");
+}
+
+if (data !== null && data !== undefined) {
+  console.log("Variable is defined");
+}
